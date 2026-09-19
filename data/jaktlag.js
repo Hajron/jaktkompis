c464c7053057a5bff8abda71a5d88088a208282e
Ronny Harjo <ronnyharjo@Mac.lan>
2026-09-19 21:41:12 +0200

    Legg til statisk datamodell for jaktlaget

---
+++ data/jaktlag.js
@@ -0,0 +1,38 @@
+export const jaktlag = [
+  {
+    id: '1',
+    navn: 'Ola Nordmann',
+    rolle: 'Poststående',
+    post: 'Storhaugen',
+    status: 'På post',
+    sistSett: '08:42',
+    telefon: '900 12 345',
+  },
+  {
+    id: '2',
+    navn: 'Kari Berg',
+    rolle: 'Hundefører',
+    post: 'Myrkanten',
+    status: 'På vei',
+    sistSett: '08:55',
+    telefon: '900 23 456',
+  },
+  {
+    id: '3',
+    navn: 'Per Lie',
+    rolle: 'Los',
+    post: 'Bekkedalen',
+    status: 'Pause',
+    sistSett: '08:30',
+    telefon: '900 34 567',
+  },
+  {
+    id: '4',
+    navn: 'Ingrid Stien',
+    rolle: 'Poststående',
+    post: 'Furumoen',
+    status: 'På post',
+    sistSett: '09:01',
+    telefon: '900 45 678',
+  },
+];
